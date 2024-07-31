e9b586e041ba4eb270a6a02d29c6b3d188a65f46
Thom Hunkin <thom.hunkin@gmail.com>
2024-07-31 13:32:48 +0000

    Added functions and event listener to run code once DOM is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-console.log("Connected!");
+// Wait for DOM to finish loading before running code
+
+document.addEventListener("DOMContentLoaded", function() {
+
+})
+
+function rollDice() {
+
+}
+
+function selectedHigher() {
+
+}
+
+function selectedLower() {
+
+}
+
+function checkResult() {
+
+}
+
+function incrementCorrect() {
+
+}
+
+function incrementIncorrect() {
+
+}
