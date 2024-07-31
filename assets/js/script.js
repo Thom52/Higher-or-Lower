55bbf7a696dc866c7330c83cce97046810fb3c5f
Thom Hunkin <thom.hunkin@gmail.com>
2024-07-31 18:29:12 +0000

    Add skeleton script for dice roll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Wait for DOM to finish loading before running code
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    rollDice();
+    const rollDiceButton = document.querySelector(".btn--roll");
+    rollDiceButton.addEventListener("click", rollDice);
 })
 
 function rollDice() {
-
+    let number = 5;
+    console.log(number);
 }
 
 function selectedHigher() {
