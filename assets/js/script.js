14e84212ebe513e755cff005c0d922645d9c1141
Thom Hunkin <thom.hunkin@gmail.com>
2024-08-02 14:42:20 +0000

    Added rollIn animation to dice img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,63 @@
-let currentRoll = 0;
+// Keeps value of last dice roll
+let previousRoll = 0;
 
 // Wait for DOM to finish loading before running code
+// Get button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
 
     rollDice();
 
     higherButton = document.querySelector(".higher");
-    higherButton.addEventListener("click", selectedHigher);
+    higherButton.addEventListener("click", selectedHigher); {
+    }
 
     lowerButton = document.querySelector(".lower");
     lowerButton.addEventListener("click", selectedLower);
-    
+
 })
 
+/**
+ * Begins main game loop. 
+ * Rolls dice on DOM load and stores value
+ * for guessing higher or lower
+ */
 function rollDice() {
 
     const num1 = Math.floor(Math.random() * 6) + 1;
-
-    currentRoll = num1;
+    console.log(num1);
 
     document.querySelector("#dice").setAttribute("src", "assets/" + "images/" +
     "d" + num1 + ".png");
+
+    previousRoll = num1;
 }
 
+/**
+ * Generates and stores a new random number
+ */
 function newDiceResult() {
 
     const num1 = Math.floor(Math.random() * 6) + 1;
-
-    const newResult = num1;
-
+    console.log(num1);
+    
     document.querySelector("#dice").setAttribute("src", "assets/" + "images/" +
     "d" + num1 + ".png");
 
-    return newResult;
+    const newResult = num1;
 
+    return newResult;
 }
 
+/**
+ * Checks the current roll against the new result
+ * looks to see if the new value is higher then the previous
+ * if so, tells the user if they guessed correctly or not
+ */
 function selectedHigher() {
 
     const newResult = newDiceResult();
 
-    if (newResult >= currentRoll) {
+    if (newResult > previousRoll) {
         alert("Congratulations!");
     } else {
         alert("Better luck next time!");
@@ -48,15 +65,20 @@ function selectedHigher() {
 
 }
 
+/**
+ * Checks the current roll against the new result
+ * looks to see if the new value is lower then the previous
+ * if so, tells the user if they guessed correctly or not
+ */
 function selectedLower() {
 
     const newResult = newDiceResult();
 
-    if (newResult <= currentRoll) {
+    if (newResult < previousRoll) {
         alert("Congratulations!");
     } else {
         alert("Better luck next time!");
-    }
+    }   
 
 }
 
@@ -67,3 +89,4 @@ function incrementCorrect() {
 function incrementIncorrect() {
 
 }
+
