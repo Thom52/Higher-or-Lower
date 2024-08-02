9dcecda46d7671b4092044aa945e7b0bd525ef92
Thom Hunkin <thom.hunkin@gmail.com>
2024-08-02 13:36:26 +0000

    Added code for higher guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,51 @@
-// Wait for DOM to finish loading before running code
+let currentRoll = 0;
 
+// Wait for DOM to finish loading before running code
 document.addEventListener("DOMContentLoaded", function() {
 
     rollDice();
 
-    const rollDiceButton = document.querySelector(".btn--roll");
-    rollDiceButton.addEventListener("click", rollDice)
-
+    higherButton = document.querySelector(".higher");
+    higherButton.addEventListener("click", selectedHigher);
+    
 })
 
 function rollDice() {
 
-    let num1 = Math.floor(Math.random() * 6) + 1;
+    const num1 = Math.floor(Math.random() * 6) + 1;
+
+    currentRoll = num1;
 
     document.querySelector("#dice").setAttribute("src", "assets/" + "images/" +
     "d" + num1 + ".png");
 }
 
-function diceResult() {
+function newDiceResult() {
+
+    const num1 = Math.floor(Math.random() * 6) + 1;
+
+    const newResult = num1;
+
+    document.querySelector("#dice").setAttribute("src", "assets/" + "images/" +
+    "d" + num1 + ".png");
+
+    return newResult;
 
 }
 
 function selectedHigher() {
 
+    const newResult = newDiceResult();
+
+    if (newResult >= currentRoll) {
+        alert("Congratulations!");
+    } else {
+        alert("Better luck next time!");
+    }
+
 }
 
 function selectedLower() {
-
-}
-
-function diceResult() {
 
 }
 
